4d038de10b7ac3428262d9ab4624d2cc12a487a9
Josiah Potts <91208436+joeshenwithyou@users.noreply.github.com>
2024-06-09 16:12:06 -0700

    Delete start-server.sh

--- start-server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-instance_id=$(terraform show -json | jq -r '.values.root_module.resources[] | select(.type=="aws_instance" and .name=="minecraft") | .values.id')
-aws ec2 start-instances --instance-ids $instance_id
-
-instance_ip=$(aws ec2 describe-instances --instance-ids $instance_id --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-echo "Minecraft server is now running at: $instance_ip"
