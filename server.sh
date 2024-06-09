ca6eb9f7cd27576797ec06520ecf0ac335c8ab00
Josiah Potts <91208436+joeshenwithyou@users.noreply.github.com>
2024-06-09 16:09:56 -0700

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,13 +1,46 @@
 #!/bin/bash
-instance_ip=$(terraform output -raw instance_ip_addr)
-ssh -i "path/to/your/private_key.pem" ubuntu@$instance_ip << 'EOF'
+
+# Retrieve the public IP address of the instance using Terraform output
+instance_public_ip=$(terraform output -json instance_public_ip | jq -r .)
+
+# Check if the IP address is obtained successfully
+if [ -z "$instance_public_ip" ]; then
+  echo "Failed to retrieve the instance public IP address."
+  exit 1
+fi
+
+# SSH into the instance and execute commands
+ssh -i /Users/pottsj/Engineering/minecraft-server-setup/minecraft-key ubuntu@$instance_public_ip << EOF
+# Commands to execute on the remote server go here
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y openjdk-17-jre-headless
-wget -O server.jar https://launcher.mojang.com/v1/objects/0b25178829b6c647a90c67d76751cf305d9a1bb7/server.jar
+# Install Java
+sudo apt install default-jre -y
+# Download and run Minecraft server
+wget https://piston-data.mojang.com/v1/objects/145ff0858209bcfc164859ba735d4199aafa1eea/server.jar -O minecraft_server.1.20.6.jar
+java -Xmx1024M -Xms1024M -jar minecraft_server.1.20.6.jar nogui
+# Sign the EULA
 echo "eula=true" > eula.txt
-nohup java -Xmx1024M -Xms1024M -jar server.jar nogui &
-exit
-EOF
+# Create Systemd service file for Minecraft
+sudo tee /etc/systemd/system/minecraft.service > /dev/null <<EOL
+[Unit]
+Description=Minecraft Server
+After=network.target
 
-echo "Minecraft server is now configured and running at: $instance_ip"
+[Service]
+User=ubuntu
+WorkingDirectory=/home/ubuntu
+ExecStart=/usr/bin/java -Xmx1024M -Xms1024M -jar /home/ubuntu/minecraft_server.jar nogui
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOL
+# Enable and start the Minecraft service
+sudo systemctl daemon-reload
+sudo systemctl enable minecraft
+sudo systemctl start minecraft
+sudo systemctl status minecraft
+
+java -Xmx1024M -Xms1024M -jar minecraft_server.1.20.6.jar nogui
+EOF
