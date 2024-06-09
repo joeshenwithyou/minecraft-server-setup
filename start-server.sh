5cac056533898e4a0e710214b6adaa14a6bfd0c0
Josiah Potts <91208436+joeshenwithyou@users.noreply.github.com>
2024-06-09 00:20:25 -0700

    Update start-server.sh

--- start-server.sh
+++ start-server.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-
-# Fetch the instance ID from Terraform state
-instance_id=$(terraform output -raw instance_id)
-
-# Start the EC2 instance
-echo "Starting the Minecraft server..."
+instance_id=$(terraform show -json | jq -r '.values.root_module.resources[] | select(.type=="aws_instance" and .name=="minecraft") | .values.id')
 aws ec2 start-instances --instance-ids $instance_id
 
-# Fetch the new public IP address after starting
 instance_ip=$(aws ec2 describe-instances --instance-ids $instance_id --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
 echo "Minecraft server is now running at: $instance_ip"
