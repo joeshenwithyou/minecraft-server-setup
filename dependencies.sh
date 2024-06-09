06c0fcbf6c376178e82cf96cd5ee88eaa4eef3b1
Josiah Potts <91208436+joeshenwithyou@users.noreply.github.com>
2024-06-09 16:09:02 -0700

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
+
 # Install Terraform
-echo "Installing Terraform..."
-curl -O https://releases.hashicorp.com/terraform/1.0.11/terraform_1.0.11_linux_amd64.zip
-unzip terraform_1.0.11_linux_amd64.zip
-sudo mv terraform /usr/local/bin/
-rm terraform_1.0.11_linux_amd64.zip
+brew install terraform
 
 # Install AWS CLI
-echo "Installing AWS CLI..."
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-rm -rf aws awscliv2.zip
-
-echo "Dependencies installed successfully."
+brew install awscli
